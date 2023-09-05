01cf16c50a24d4f017447e097afdf029965b918c
SeungGukYoo <dbtmd878@gmail.com>
2023-09-05 22:41:21 +0900

    feat: 리스트와 인풋 컴포넌트 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,15 @@
+import Input from './components/Input';
+import List from './components/List';
+import { DisplayProvider } from './context/DisplayListContext';
+
 function App() {
   return (
-    <>
-      <h1>APP</h1>
-    </>
+    <div className="max-w-[1024px] mx-auto mt-10 bg-slate-400 flex justify-center items-center flex-col ">
+      <DisplayProvider>
+        <Input />
+        <List />
+      </DisplayProvider>
+    </div>
   );
 }
 
