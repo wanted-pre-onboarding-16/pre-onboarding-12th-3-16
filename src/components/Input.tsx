ccdd0054af38885dc34c9dcf37e90fe2578fd7fe
SeungGukYoo <dbtmd878@gmail.com>
2023-09-06 02:11:36 +0900

    feat: debounce 방식을 통한 API호출 기능 추가

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,11 +1,18 @@
 import { useDisplay } from '../context/DisplayListContext';
+import useInput from '../hook/useInput';
 
 function Input() {
   const targetRef = useDisplay()?.targetRef;
 
+  const { onChangeValue } = useInput();
+
+  const ApiTrigger = () => {
+    if (targetRef?.current) onChangeValue(targetRef?.current.value);
+  };
+
   return (
     <div>
-      <input className="border border-blue-600" ref={targetRef} type="text" />
+      <input className="border border-blue-600" ref={targetRef} type="text" onChange={ApiTrigger} />
       <button>SEARCH</button>
     </div>
   );
