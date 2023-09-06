2a66366ec9546452add98100f4748cbe1166e1ed
SeungGukYoo <dbtmd878@gmail.com>
2023-09-07 02:07:32 +0900

    feat: 키보드 이동시 아이템 선택 기능 추가

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,11 +1,64 @@
+import { useEffect, useState } from 'react';
 import { useDiseasStore } from '../context/DiseaseStoreContext';
 import { useDisplay } from '../context/DisplayListContext';
+import ListItem from './ListItem';
 
 function List() {
-  const isFocused = useDisplay()?.isFocused;
+  const display = useDisplay();
   const data = useDiseasStore();
+  const [isOnIndex, setIsOnIndex] = useState(-2);
+  useEffect(() => {
+    const moveList = (e: KeyboardEvent) => {
+      e.stopPropagation();
 
-  return isFocused && <ul>{data?.map(el => <li key={el.sickCd}>{el.sickNm}</li>)}</ul>;
+      switch (e.code) {
+        case 'ArrowUp':
+          setIsOnIndex(pre => {
+            if (pre <= 0) return (pre = 0);
+            else pre -= 1;
+            return pre;
+          });
+          break;
+        case 'ArrowDown':
+          setIsOnIndex(pre => {
+            if (!data?.length) return pre;
+            if (pre === data?.length - 1) return pre;
+            else pre += 1;
+            return pre;
+          });
+          break;
+        default:
+          return;
+      }
+    };
+
+    if (display?.isFocused) {
+      console.log('add');
+
+      window.addEventListener('keyup', moveList);
+    } else {
+      console.log('remove');
+      window.removeEventListener('keyup', moveList);
+    }
+
+    return () => {
+      window.removeEventListener('keyup', moveList);
+    };
+  }, [data?.length, display?.isFocused]);
+  console.log(isOnIndex, data?.length);
+  return (
+    display?.isFocused && (
+      <ul>
+        {data?.length === 0 ? (
+          <li>검색어가 없습니다.</li>
+        ) : (
+          data?.map((el, idx) => (
+            <ListItem key={el.sickCd} data={el.sickNm} isOn={idx === isOnIndex} />
+          ))
+        )}
+      </ul>
+    )
+  );
 }
 
 export default List;
