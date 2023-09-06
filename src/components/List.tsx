85a4d61af3612fa3876a772fb4e47bac66bf913b
SeungGukYoo <dbtmd878@gmail.com>
2023-09-06 15:58:44 +0900

    feat: 실제 API 데이터 출력

--- src/components/List.tsx
+++ src/components/List.tsx
@@ -1,16 +1,11 @@
+import { useDiseasStore } from '../context/DiseaseStoreContext';
 import { useDisplay } from '../context/DisplayListContext';
 
 function List() {
   const isFocused = useDisplay()?.isFocused;
+  const data = useDiseasStore();
 
-  return (
-    isFocused && (
-      <ul>
-        <li>결과</li>
-        <li>결과</li>
-      </ul>
-    )
-  );
+  return isFocused && <ul>{data?.map(el => <li key={el.sickCd}>{el.sickNm}</li>)}</ul>;
 }
 
 export default List;
