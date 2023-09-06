9de487a66269d3cefc03060848edabb7ceb80c38
SeungGukYoo <dbtmd878@gmail.com>
2023-09-06 15:56:45 +0900

    feat: 질병 정보를 관리하기 위한 전역상태 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import Input from './components/Input';
 import List from './components/List';
+import { DiseasStoreProvider } from './context/DiseaseStoreContext';
 import { DisplayProvider } from './context/DisplayListContext';
 
 function App() {
   return (
     <div className="max-w-[1024px] mx-auto mt-10 bg-slate-400 flex justify-center items-center flex-col ">
       <DisplayProvider>
-        <Input />
-        <List />
+        <DiseasStoreProvider>
+          <Input />
+          <List />
+        </DiseasStoreProvider>
       </DisplayProvider>
     </div>
   );
