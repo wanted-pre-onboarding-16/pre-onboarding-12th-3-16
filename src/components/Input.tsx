a5374ce4fedfa54db25a00473892a1415ca27e1c
SeungGukYoo <dbtmd878@gmail.com>
2023-09-07 18:23:52 +0900

    refactor: 전역에서 제거한 블러,포커스 이벤트로 인한 코드 수정

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -1,18 +1,30 @@
+import { useRef } from 'react';
 import { useDisplay } from '../context/DisplayListContext';
 import useInput from '../context/hook/useInput';
 
 function Input() {
-  const targetRef = useDisplay()?.targetRef;
-
+  const inputRef = useRef<HTMLInputElement>(null);
   const onChangeValue = useInput()?.onChangeValue;
-
+  const isWriting = useDisplay()?.isWriting;
   const ApiTrigger = () => {
-    if (targetRef?.current && onChangeValue) onChangeValue(targetRef?.current.value);
+    if (inputRef.current && onChangeValue) {
+      onChangeValue(inputRef.current.value);
+    }
+  };
+  const isFoucsInput = (e: React.FocusEvent<HTMLInputElement>) => {
+    isWriting && isWriting(e.type);
   };
 
   return (
     <div>
-      <input className="border border-blue-600" ref={targetRef} type="text" onChange={ApiTrigger} />
+      <input
+        className="border border-blue-600"
+        ref={inputRef}
+        onChange={ApiTrigger}
+        onFocus={isFoucsInput}
+        onBlur={isFoucsInput}
+        type="text"
+      />
       <button>SEARCH</button>
     </div>
   );
